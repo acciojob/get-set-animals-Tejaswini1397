99614ae7c5ce23a2db3e5cbc843f0fd7bd879fc6
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-16 20:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ class Animal {
 
 class Dog extends Animal {
 	bark() {
-        console.log("Woof");
+        console.log("woof");
     }
 }
 
 class Cat extends Animal {
 	 purr() {
-        console.log("Purr");
+        console.log("purr");
     }
 }
 
