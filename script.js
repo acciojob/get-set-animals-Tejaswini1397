e3157c54f88dd4cb6913db6ef92ca4dc93cb81b8
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-16 20:38:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //complete this code
 class Animal {
-	constructer(species){
-		this.species1=species;
-	}
-	get species() {
-        return this.species1;
-    }
-	 makeSound() {
-        console.log("The Siamese makes a sound");
-    }
+	constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
 }
 
 class Dog extends Animal {
